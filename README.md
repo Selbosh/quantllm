2745cda2fc278231c55f0129ff7ed290a7d68842
Sergey Redyuk <sergred@gmail.com>
2023-11-20 17:55:57 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 # llms-for-quantitative-knowledge-retrieval
 
+## Installation
+
+Install [pyenv](https://github.com/pyenv/pyenv) and [poetry](https://python-poetry.org/) for environment management. In the `pyproject.toml`, see line `python = ...` for the acceptable Python version, e.g., `">=3.9,<3.10"`, and do the following from the root directory of the project 
+```
+pyenv install <python version>
+pyenv local <python version>
+poetry env use <python version>
+poetry install
+```
+e.g., `<python version>` is equal to 3.9.17
+
+For missing packages, use `poetry add <package name>` (see [poetry docs](https://python-poetry.org/)).
+
+## Get data
+
+```
+poetry run python scripts/get-datasets.py 
+```
+
 
 
 ## Getting started
@@ -91,3 +110,4 @@ For open source projects, say how it is licensed.
 
 ## Project status
 If you have run out of energy or time for your project, put a note at the top of the README saying that development has slowed down or stopped completely. Someone may choose to fork your project or volunteer to step in as a maintainer or owner, allowing your project to keep going. You can also make an explicit request for maintainers.
+
---
+++ scripts/get-datasets.py
@@ -0,0 +1,43 @@
+from sklearn.datasets import fetch_openml
+from pathlib import Path
+import pandas as pd
+import openml
+import sys
+
+openml.config.apikey = 'f44f61a5ad260b7dc3a501de448e47ce'
+
+folder = Path(__file__).parents[1] / 'data/'
+folder.mkdir(parents=True, exist_ok=True)
+
+to_save = folder / 'openml-datasets-CC18.csv'
+if to_save.exists():
+    datasets = pd.read_csv(to_save)
+else:
+    # benchmark = openml.study.get_suite('OpenML-CC18')
+    # datasets = benchmark.data
+    datasets = openml.datasets.list_datasets(tag='OpenML-CC18', output_format='dataframe')
+    datasets.to_csv(to_save, index=False)
+
+print(datasets)
+
+
+for _, row in datasets.iterrows():
+    did = int(row['did'])
+    if all(map(lambda x: Path(folder / f'openml/{did}/{x}.csv').exists(), ['X', 'y'])):
+        X = pd.read_csv(folder / f'openml/{did}/X.csv')
+        y = pd.read_csv(folder / f'openml/{did}/y.csv')
+    else:
+        Path(folder / f'openml/{did}/').mkdir(exist_ok=True, parents=True)
+        X, y = fetch_openml(data_id=did, return_X_y=True, as_frame=True, parser='auto')
+        X.to_csv(folder / f'openml/{did}/X.csv', index=False)
+        y.to_csv(folder / f'openml/{did}/y.csv', index=False)
+
+    dataset = openml.datasets.get_dataset(did, download_data=True, download_qualities=True, download_features_meta_data=True)
+
+    print(dataset)
+    print(dataset.description)
+    for k, v in dataset.features.items():
+        missing = f"{v.number_missing_values} missing values" if v.number_missing_values else ""
+        print(f"{v.name} ({v.data_type}), {v.nominal_values} {missing}")
+    for k, q in dataset.qualities.items():
+        if not pd.isna(q): print(k, q)
