32f93d646ed179ea218a3390a8544d1ddd326bf9
Yuichiro Iwashita <9864901+yiRMT@users.noreply.github.com>
2023-12-08 20:30:07 +0100

    Updated get-datasets.py
    - added dataset descriptions

--- scripts/get-datasets.py
+++ scripts/get-datasets.py
@@ -6,7 +6,7 @@ import sys
 
 openml.config.apikey = 'f44f61a5ad260b7dc3a501de448e47ce'
 
-folder = Path(__file__).parents[1] / 'data/'
+folder = Path(__file__).parents[1] / 'data/openml/'
 folder.mkdir(parents=True, exist_ok=True)
 
 to_save = folder / 'openml-datasets-CC18.csv'
@@ -22,14 +22,17 @@ print(datasets)
 
 for _, row in datasets.iterrows():
     did = int(row['did'])
-    if all(map(lambda x: Path(folder / f'openml/{did}/{x}.csv').exists(), ['X', 'y'])):
-        X = pd.read_csv(folder / f'openml/{did}/X.csv')
-        y = pd.read_csv(folder / f'openml/{did}/y.csv')
+    if all(map(lambda x: Path(folder / f'{did}/{x}.csv').exists(), ['X', 'y'])):
+        X = pd.read_csv(folder / f'{did}/X.csv')
+        y = pd.read_csv(folder / f'{did}/y.csv')
     else:
-        Path(folder / f'openml/{did}/').mkdir(exist_ok=True, parents=True)
-        X, y = fetch_openml(data_id=did, return_X_y=True, as_frame=True, parser='auto')
-        X.to_csv(folder / f'openml/{did}/X.csv', index=False)
-        y.to_csv(folder / f'openml/{did}/y.csv', index=False)
+        Path(folder / f'{did}/').mkdir(exist_ok=True, parents=True)
+        data = fetch_openml(data_id=did, return_X_y=False, as_frame=True, parser='auto')
+        X, y, description = data['data'], data['target'], data['DESCR']
+        X.to_csv(folder / f'{did}/X.csv', index=False)
+        y.to_csv(folder / f'{did}/y.csv', index=False)
+        with open(folder / f'{did}/description.txt', 'w') as f:
+            f.write(str(description))
 
     dataset = openml.datasets.get_dataset(did, download_data=True, download_qualities=True, download_features_meta_data=True)
 
