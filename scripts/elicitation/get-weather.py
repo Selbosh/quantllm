a43071259b693682d182c8d7d2ce92182abb87e7
David Selby <selby@rptu.de>
2024-01-31 08:05:51 +0100

    Rename weather getter

--- scripts/elicitation/get-weather.py
+++ scripts/elicitation/get-weather.py
@@ -17,10 +17,12 @@ weather.get_cities_weather()
 dataset = weather.data
 dataset.to_csv(folder / 'cities.csv', index=False)
 
+# Commenting out for now because the dataset doesn't seem to work:
+# ----------------------------------------------------------------
 # Download mean BMI estimates by country from the World Health Organization:
 # https://www.who.int/data/gho/data/indicators/indicator-details/GHO/mean-bmi-(kg-m-)-(age-standardized-estimate)
-who_gho_file = folder / 'who_bmi.csv'
-if not who_gho_file.exists():
-  print('Please download the following dataset from the WHO Global Health Observatory\n'
-        'https://www.who.int/data/gho/data/indicators/indicator-details/GHO/mean-bmi-(kg-m-)-(age-standardized-estimate)\n'
-        'Then save it in path: ' + str(who_gho_file))
+# who_gho_file = folder / 'who_bmi.csv'
+# if not who_gho_file.exists():
+#   print('Please download the following dataset from the WHO Global Health Observatory\n'
+#         'https://www.who.int/data/gho/data/indicators/indicator-details/GHO/mean-bmi-(kg-m-)-(age-standardized-estimate)\n'
+#         'Then save it in path: ' + str(who_gho_file))
